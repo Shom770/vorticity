217da782922140f55d8d90946c215ac16b8743e7
Shom770 <82843611+Shom770@users.noreply.github.com>
2021-08-11 18:14:50 -0400

    add mongodb link

--- cogs/currency.py
+++ cogs/currency.py
@@ -1,12 +1,15 @@
 import discord
 from discord.ext import commands
-from discord.ext.commands import Context
-
+from discord.ext.commands import Context, BucketType
+from pymongo import MongoClient
+import pymongo
+from config import mongodb_link
 
 class Currency(commands.Cog):
     """Cog for currency system"""
     def __init__(self, bot):
         self.bot = bot
+        self.db = MongoClient(mongodb_link)['Data']
 
 
 def setup(bot):
