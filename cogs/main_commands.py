cd091e8bea9c4a7e6752c3e267fcafe04cab2e13
Shom770 <82843611+Shom770@users.noreply.github.com>
2021-08-11 18:15:07 -0400

    add command error handling to main_commands.py

--- cogs/main_commands.py
+++ cogs/main_commands.py
@@ -7,6 +7,17 @@ class MainCommands(commands.Cog):
     """Cog for all the main commands."""
     def __init__(self, bot):
         self.bot = bot
+
+    @staticmethod
+    def error_embed(message: str, description: str = '', color: discord.Color = discord.Color.red()):
+        err_embed = discord.Embed(title=message, color=color)
+        if description:
+            err_embed.description = description
+        return err_embed
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx: Context, exception: Exception):
+        await ctx.send(embed=self.error_embed("Slow your horses!", str(exception)))
         
 
 def setup(bot):
