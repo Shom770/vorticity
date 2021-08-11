29e2143153bd83171c73c0fedd1fa11448d7f844
Shom770 <82843611+Shom770@users.noreply.github.com>
2021-08-11 18:37:12 -0400

    added ``wallet`` command which gets the balance of the user

--- cogs/currency.py
+++ cogs/currency.py
@@ -3,13 +3,43 @@ from discord.ext import commands
 from discord.ext.commands import Context, BucketType
 from pymongo import MongoClient
 import pymongo
+from bisect import insort
 from config import mongodb_link
 
 class Currency(commands.Cog):
     """Cog for currency system"""
     def __init__(self, bot):
         self.bot = bot
-        self.db = MongoClient(mongodb_link)['Data']
+        self.db = MongoClient(mongodb_link)
+
+    @commands.command()
+    async def wallet(self, ctx: Context, member: discord.Member = None):
+        server_col = self.db[str(ctx.guild.id)]
+        if member:
+            name = member.display_name
+            member = member.id
+        else:
+            member = ctx.author.id
+            name = ctx.author.display_name
+
+        if str(member) not in (collections := server_col.collection_names()):
+            new_col = server_col[str(member)]
+            ranks = [server_col[col].find_one()['balance'] for col in collections]
+            insort(ranks, 250)
+            try:
+                rank = ranks.index(250) + 1
+            except ValueError:
+                rank = 1
+            new_col.insert_one({'_id': len(collections) + 1,
+                                'balance': 250,
+                                'rank': rank})
+
+        info = server_col[str(member)].find_one()
+        wallet_embed = discord.Embed(title=f"{name}'s wallet", color=discord.Color.green())
+        wallet_embed.add_field(name="Balance", value=f"{info['balance']} ⛂", inline=False)
+        wallet_embed.add_field(name="Server Rank", value=info['rank'], inline=False)
+
+        await ctx.send(embed=wallet_embed)
 
 
 def setup(bot):
