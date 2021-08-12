d8d0f7a2a3402773ff40cd3f93b00cf0faec5e9f
Shom770 <82843611+Shom770@users.noreply.github.com>
2021-08-11 20:19:55 -0400

    add configuring for store permissions

--- cogs/currency.py
+++ cogs/currency.py
@@ -9,32 +9,45 @@ from config import mongodb_link
 
 class Currency(commands.Cog):
     """Cog for currency system"""
+
     def __init__(self, bot):
         self.bot = bot
         self.db = MongoClient(mongodb_link)
 
     @commands.command(aliases=['configure'])
     @commands.has_permissions(administrator=True)
-    async def config(self, ctx: Context, params: str = '') -> None:
+    async def config(self, ctx: Context, *, params: str = '') -> None:
         """Configure the currency system in the server"""
         if not params:
             help_embed = discord.Embed(title=f"Configure {ctx.guild.name}'s currency system",
                                        description="An embed of all the settings you can configure for this "
                                                    "currency system.", color=discord.Color.orange())
             help_embed.set_thumbnail(url=ctx.guild.icon_url)
-            help_embed.add_field(name="Add a name to the currency",
-                                 value=self.bot.command_prefix + "config name={name you want}")
+            help_embed.add_field(name="**Add a name to the currency**",
+                                 value='``' + self.bot.command_prefix + "config name=[name you want]``")
+            help_embed.add_field(name="**Set permissions for who can use this server's market**",
+                                 value='``' + self.bot.command_prefix + "config store=(role=[minimum role needed] - "
+                                                                        "rank=[minimum server rank by balance needed] -"
+                                                                        " balance=[minimum balance needed])``")
             await ctx.send(embed=help_embed)
         else:
             info_col = self.db[str(ctx.guild.id)]['info']
             config_info = info_col.find_one()
             if len(info_col.find_one().keys()) <= 1:
-                info_col.insert_one({'currency_name': 'dollars', 'store': None})
+                info_col.insert_one({'currency_name': 'dollars', 'store': {'role': None, 'rank': None, 'balance': 250}})
             currency_name, store = config_info['currency_name'], config_info['store']
-            params = dict([param.split('=') for param in params.split(' ')])
+            params = dict([[ele for ele in param.partition('=') if ele != '=']
+                          for param in params.split(',' if ', ' not in params else ', ')])
             currency_name = params.get('name', currency_name)
+            store = params.get('store', store)
+            if '=' in store:
+                store = store.replace('(', '').replace(')', '').split('-' if ' - ' not in store else ' - ')
+                store = dict([element.split('=') for element in store])
+                for key in ('role', 'rank', 'balance'):
+                    if key not in store.keys():
+                        store[key] = None
             info_col.update_one(info_col.find_one(), {'$set': {'currency_name': currency_name,
-                                                               'store': None}})
+                                                               'store': store}})
             await ctx.send(embed=discord.Embed(title="Success!", color=discord.Color.green()))
 
     @commands.command(aliases=["balance", "bal", "wal"])
@@ -44,7 +57,7 @@ class Currency(commands.Cog):
         server_col = self.db[str(ctx.guild.id)]
         info_col = server_col['info']
         if len(info_col.find_one().keys()) <= 1:
-            info_col.insert_one({'currency_name': 'dollars', 'store': None})
+            info_col.insert_one({'currency_name': 'dollars', 'store': {'role': None, 'rank': None, 'balance': 250}})
         if member:
             name = member.display_name
             member = member.id
