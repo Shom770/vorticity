c8d1319db4e36238ba09d1716bfb1415d63c3f00
Shom770 <82843611+Shom770@users.noreply.github.com>
2021-08-12 21:15:03 -0400

    uncomment parts of code'

--- cogs/main_commands.py
+++ cogs/main_commands.py
@@ -16,13 +16,13 @@ class MainCommands(commands.Cog):
             err_embed.description = description
         return err_embed
 
-    # @commands.Cog.listener()
-    # async def on_command_error(self, ctx: Context, exception: Exception) -> None:
-    #     """
-    #     When an error is thrown, this command catches it and will send an error embed regarding:
-    #         - cooldown errors
-    #     """
-    #     await ctx.send(embed=self.error_embed("Slow your horses!", str(exception)))
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx: Context, exception: Exception) -> None:
+        """
+        When an error is thrown, this command catches it and will send an error embed regarding:
+            - cooldown errors
+        """
+        await ctx.send(embed=self.error_embed("Slow your horses!", str(exception)))
         
 
 def setup(bot):
